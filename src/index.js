776147897bbf7be5102dbb96c6e3c39eb28f230c
Daniellakalu363 <daniellakalu4@gmail.com>
2024-06-26 02:10:04 -0700

    adding all files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// alert("My name is Daniella Kalu");
+// let name1 =prompt("Tell me your name?");
+// alert("Hi " + name1+", welcome to our website")
+// alert(`Hi ${name1}, from ${hometown},age${years} welcome to our website`)
+
+
+let title =prompt("What is your title?")
+let name2 =prompt("Please tell me your name?")
+let product =prompt("What is your product?")
+alert("Hi " + name2 +", welcome to our website enjoy this page😊🤙🏽")
